c44fa15064cf7262ed05d0bd07a853dfb2119748
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-01 02:44:30 +0900

    入力された数字の絶対値を出力

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, char *argv[]) 
 {
     char str[5];
-    
+    int array[100];
+    int i=0;
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int tmp = strtol(str,NULL,10);
+        printf("%d \n", abs(tmp));
     }
 
     return 0;
