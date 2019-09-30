0b4a96907966d4571974e9e7cc0d4582460451dd
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-01 03:11:37 +0900

    入力された数によって処理を分岐

--- main.c
+++ main.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 int main(int argc, char *argv[]) 
 {
-    char str[5];
+    char str[255];
     int array[100];
     int i=0;
     while(fgets(str, sizeof(str), stdin)){
         int tmp = strtol(str,NULL,10);
-        printf("%d \n", abs(tmp));
+        if(tmp>=0)
+        {
+            array[i]=tmp;
+            i++;
+        }
+        else
+        {
+            printf("%d \n",array[ abs(tmp)-1]);
+        }
     }
 
     return 0;
