dc9624d65c9f74513f10e8b93bc3caa2d63d3041
yasu2520 <52170130+yasu2520@users.noreply.github.com>
2019-09-30 17:33:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,28 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    int num, count = 0;
+    int* array = mallloc(sizeof(int) * 100);
+    while(fgets(str, sizeof(str), stdin))
+    {
+        num = atoi(str);
+
+        if(num >= 0 && num <= 100)
+        {
+            if(count < 100)
+            { 
+                array[count] = num;
+                count++;
+            }
+            else
+                printf("配列がいっぱいです。¥n");
+        }
+        else if(num < 0 && num >= 100)
+            printf("%dが格納されています。¥n",array[-count - 1]);
+        else
+            printf("不正な入力です。¥n");
     }
 
+    free(array);
     return 0;
 }
