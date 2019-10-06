5dc638d7b28da193774c182343e2072611849d04
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2019-10-07 00:19:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 
 int main()
 {
-	int str[5],i=0,n,j;
-	while (1){
-			scanf_s("%d",&str[i]);
-			if (str[i] < 0) {
+	int str[5], i = 0,c, n, j;
+	while (1) {
+		scanf_s("%d", &str[i]);
+		c = str[i];
+		if (0 <= c && c <= 100)
+			i++;
+		else if (str[i] < 0) {
 				str[i] *= -1;
 				n = str[i];
-				for (j = 0; j < n; j++){
+				for (j = 0; j < n; j++) {
 					printf("%d\n", str[j]);
 				}
 				break;
 			}
-			i++;
+		
+		
 	}
 	return 0;
 }
