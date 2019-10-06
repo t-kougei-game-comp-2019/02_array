f3515c59e656d2ab7027279f6ea339b2a8c9f4ff
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2019-10-06 23:55:28 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
-{
-    char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
 
-    return 0;
+int main()
+{
+	int str[5],i=0,n,j;
+	while (1){
+			scanf_s("%d",&str[i]);
+			if (str[i] < 0) {
+				str[i] *= -1;
+				n = str[i];
+				for (j = 0; j < n; j++){
+					printf("%d\n", str[j]);
+				}
+				break;
+			}
+			i++;
+	}
+	return 0;
 }
