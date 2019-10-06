99eaa40395ffcb8945fe80a57f2d914bcf323c95
g1927046 <55736489+g1927046@users.noreply.github.com>
2019-10-07 00:36:24 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char* argv[])
 {
-    char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[5];
 
-    return 0;
+	while (fgets(str, sizeof(str), stdin)) {
+		printf("%s", str);
+	}
+	int num, count = 0;
+	int* array = mallloc(sizeof(int) * 100);
+
+	while (fgets(str, sizeof(str), stdin))
+	{
+		num = atoi(str);
+		if (num >= 0 && num <= 100)
+		{
+			if (count < 100) {
+				array[count] = num;
+				count++;
+			}
+			else
+				printf("配列外です\n");
+		}
+		else if (num < 0 && num >= 100)
+			printf("%dが格納してあります。\n", array[-count - 1]);
+		else
+			printf("正しく入力してください。\n");
+	}
+	return 0;
 }
