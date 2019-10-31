a79098182b6a6928d1292f5b03ff5563b6d2acb4
0zw <55938778+0zw@users.noreply.github.com>
2019-10-31 15:06:22 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+#include<stdlib.h>
 
-int main(int argc, char *argv[]) 
+
+int main(int argc, char* argv[])
+
 {
-    char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
 
-    return 0;
+	int array[100];
+	int index = 1;
+
+	char str[5];
+	int n;
+
+
+	while (fgets(str, sizeof(str), stdin)) {
+		n = atoi(str);
+		if (n >= 0) {
+
+
+			array[index++] = n;
+			
+		}
+		else {
+			printf("%d\n", array[-n]);
+
+		}
+
+	}
+
+
+
+
+	return 0;
+
 }
