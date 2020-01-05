f5802cb0dc7006f4eb9508d5c8dfb11ca1db2ed7
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2020-01-05 23:10:34 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,14 +5,17 @@ int main(int argc, char *argv[])
 	char str[5];
 	int array[100],count=0;
 	
-	while (fgets(str, sizeof(str), stdin)) {
+	while (fgets(str, sizeof(str), stdin))
+	{
 		int c = atoi(str);
 		
-		if (c>=0) {
+		if (c>=0)
+		{
 			array[count]=c;
 			count++;
 		}
-		else if(-c<=count){
+		else if(-c<=count)
+		{
 			printf("%d\n",array[-c-1]);
 		}	
 	}
