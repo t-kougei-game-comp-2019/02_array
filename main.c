21d57f97965a5574df0b8fe24a1428469ae9123b
strawberry0325 <56204933+strawberry0325@users.noreply.github.com>
2020-01-20 01:13:05 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,16 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    int array[100];
+    int input,count=0;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        input=atoi(str);
+        if(input>=0)
+            array[count]=input;
+        else
+            printf("%d",array[-input-1]);
+        count++;
     }
 
     return 0;
